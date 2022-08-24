b942cede07c475e091adb50be827e3b3d84309f3
HarperYun <harper9981@gmail.com>
2022-08-24 23:51:35 +0800

    會員列表 0824

--- src/pages/administrator/MembersPage.vue
+++ src/pages/administrator/MembersPage.vue
@@ -1,31 +1,30 @@
 <!-- 會員管理 -->
 <template>
-<div class="window-width text-center">
-  <h5>會員管理</h5>
-</div>
+  <div class="window-width text-center">
+    <h5>會員管理</h5>
+  </div>
 
-  <!-- <q-list separator bordered class="q-pa-xs q-ma-none" style="max-width: 100vw;" v-if='users.length > 0'>
+  <q-list separator bordered class="q-pa-xs q-ma-none" style="max-width: 100vw;" v-if='users.length > 0'>
 
     <q-item>
-      <q-item-section >頭像</q-item-section>
-      <q-item-section >會員ID</q-item-section>
-      <q-item-section >會員信箱</q-item-section>
-      <q-item-section >會員帳號</q-item-section>
-      <q-item-section > </q-item-section>
-      <q-item-section >操作</q-item-section>
+      <!-- <q-item-section>頭像</q-item-section> -->
+      <q-item-section>會員ID</q-item-section>
+      <q-item-section>會員信箱</q-item-section>
+      <q-item-section>會員帳號</q-item-section>
+      <q-item-section>操作</q-item-section>
     </q-item>
 
     <q-item clickable v-ripple class="q-ma-none" v-for='user in users' :key='user._id'>
-      <q-item-section >{{ user._id }}</q-item-section>
-      <q-item-section >{{ user.email }}</q-item-section>
-      <q-item-section >{{ user.account }}</q-item-section>
+      <q-item-section>{{ user._id }}</q-item-section>
+      <q-item-section>{{ user.email }}</q-item-section>
+      <q-item-section>{{ user.account }}</q-item-section>
     </q-item>
 
   </q-list>
 
   <q-list v-else class="text-center">
     <p>沒有會員</p>
-  </q-list> -->
+  </q-list>
 
 </template>
 
@@ -38,9 +37,10 @@ const users = reactive([])
 
 const init = async () => {
   try {
-    const { data } = await apiAuth.get('/users')
+    const { data } = await apiAuth.get('/users/all')
+    users.splice(0, users.length)
     users.push(...data.result)
-    console.log(users)
+    // console.log(users)
   } catch (error) {
     console.log(error)
     Swal.fire({
