f3051bfb649c8b64c2aef5b8da26c692dcd882c0
HarperYun <harper9981@gmail.com>
2022-08-24 16:36:21 +0800

    取得會員列表未完成

--- src/pages/administrator/MembersPage.vue
+++ src/pages/administrator/MembersPage.vue
@@ -4,31 +4,28 @@
   <h5>會員管理</h5>
 </div>
 
-  <q-list separator bordered class="q-pa-xs q-ma-none" style="max-width: 100vw;" v-if='orders.length > 0'>
+  <!-- <q-list separator bordered class="q-pa-xs q-ma-none" style="max-width: 100vw;" v-if='users.length > 0'>
 
     <q-item>
-      <q-item-section >訂單ID</q-item-section>
-      <q-item-section >下單日期</q-item-section>
-      <q-item-section >合計</q-item-section>
-      <q-item-section >下單帳號</q-item-section>
-      <q-item-section >訂單內容</q-item-section>
+      <q-item-section >頭像</q-item-section>
+      <q-item-section >會員ID</q-item-section>
+      <q-item-section >會員信箱</q-item-section>
+      <q-item-section >會員帳號</q-item-section>
+      <q-item-section > </q-item-section>
+      <q-item-section >操作</q-item-section>
     </q-item>
 
-    <q-item clickable v-ripple class="q-ma-none" v-for='order in orders' :key='order._id'>
-      <q-item-section >{{ order._id }}</q-item-section>
-      <q-item-section >{{ new Date(order.date).toLocaleDateString() }}</q-item-section>
-      <q-item-section >$ {{ order.totalPrice }}</q-item-section>
-      <q-item-section >{{ order.user.account }}</q-item-section>
-      <q-item-section v-for='product in order.products' :key='product._id' >
-        {{ product.product.name }} x {{ product.quantity }} （{{ product.remark }}）
-      </q-item-section>
+    <q-item clickable v-ripple class="q-ma-none" v-for='user in users' :key='user._id'>
+      <q-item-section >{{ user._id }}</q-item-section>
+      <q-item-section >{{ user.email }}</q-item-section>
+      <q-item-section >{{ user.account }}</q-item-section>
     </q-item>
 
   </q-list>
 
   <q-list v-else class="text-center">
     <p>沒有會員</p>
-  </q-list>
+  </q-list> -->
 
 </template>
 
@@ -37,24 +34,19 @@ import { reactive } from 'vue'
 import { apiAuth } from '@/boot/axios'
 import Swal from 'sweetalert2'
 
-const orders = reactive([])
+const users = reactive([])
 
 const init = async () => {
   try {
-    const { data } = await apiAuth.get('/orders/all')
-    // console.log(...data.result)
-    orders.push(...data.result.map(order => {
-      order.totalPrice = order.products.reduce((a, b) => {
-        return a + b.product.price * b.quantity
-      }, 0)
-      return order
-    }))
+    const { data } = await apiAuth.get('/users')
+    users.push(...data.result)
+    console.log(users)
   } catch (error) {
     console.log(error)
     Swal.fire({
       icon: 'error',
       title: '失敗',
-      text: '無法取得訂單1'
+      text: '無法取得會員列表'
     })
   }
 }
