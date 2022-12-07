4f468385779dfc6f2c1244abf25a1b6637deca3e
HarperYun <101688061+HarperYun@users.noreply.github.com>
2022-12-07 10:04:28 +0000

    Deploying to gh-pages from @ HarperYun/test-qs5@281ff5e0d737ebe8596153d6b5d0ddbd323faa99 🚀

--- assets/AdminPage.87fb4326.js
+++
@@ -1,1 +0,0 @@
-import{ao as t,U as o,ap as s,_ as e}from"./index.338dedac.js";const n={},c={class:"window-width text-center"},_=e("h5",null,[e("b",null,"\u5F8C\u53F0\u7BA1\u7406")],-1),a=e("h6",null,"\u8ACB\u9078\u64C7\u7BA1\u7406\u9805\u76EE",-1),r=[_,a];function d(i,l){return o(),s("div",c,r)}var f=t(n,[["render",d]]);export{f as default};
